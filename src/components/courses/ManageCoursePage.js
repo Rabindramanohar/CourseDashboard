5eddfa0db34b05ec8c38c559aa37f4b33540bf7d
rabindra <robinmanohar@gmail.com>
2021-02-24 11:18:18 +0530

    change managecoursepage class component to functional component

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,38 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import {loadCourses} from "./../../redux/actions/courseActions";
 import {loadAuthors} from "./../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidUpdate() {
-    // console.log("checking.......");
-    const {courses, authors, loadAuthors, loadCourses} = this.props;
+function ManageCoursePage({courses, authors, loadAuthors, loadCourses}) {
+  
+  useEffect(() => {
     if(courses.length == 0) {
-      loadCourses().catch((error) => {
-      alert("Loading course failed: " + error);
-    });
-  }
-
-  if(authors.length == 0) {
-    loadAuthors().catch((error) => {
-      alert("Loading authors failed: " + error);
-    });
-  }
-
-  }
-
-  componentWillUnmount() {
-    this.props.loadCourses();
-    this.props.loadAuthors();
-  }
-  render() {
+        loadCourses().catch((error) => {
+        alert("Loading course failed: " + error);
+      });
+    }
+  
+    if(authors.length == 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed: " + error);
+      });
+    }
+  }, [])
     return (
       <>
         <h2>Manage Courses</h2>
       </>
     );
-  }
 }
 
 ManageCoursePage.propTypes = {
